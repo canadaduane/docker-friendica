3436b523be9dffb7cfcc884c5ea94c0abaa5b26e
Duane Johnson <duane.johnson@gmail.com>
2014-09-04 23:36:54 -0600

    Create .htconfig.php

--- run.sh
+++ run.sh
@@ -11,8 +11,7 @@ touch /logs/friendica.log
 chmod 666 /logs/*.log
 
 if [ ! -f /app/.htconfig.php ]; then
-tee /app/.htconfig.php <<EOF
-
+echo <<EOF >/app/.htconfig.php
 <?php
 
 \$db_host = 'localhost';
@@ -31,19 +30,14 @@ tee /app/.htconfig.php <<EOF
 
 \$a->config['max_import_size'] = 200000;
 
-\$a->config['system']['maximagesize'] = 800000;
-
 \$a->config['php_path'] = '/usr/bin/php';
 
+\$a->config['system']['maximagesize'] = 800000;
 \$a->config['system']['directory_submit_url'] = ''; # 'http://dir.friendica.com/submit';
 \$a->config['system']['directory_search_url'] = 'http://dir.friendica.com/directory?search=';
-
 \$a->config['system']['huburl'] = 'http://pubsubhubbub.appspot.com';
-
 \$a->config['system']['rino_encrypt'] = true;
-
 \$a->config['system']['theme'] = 'duepuntozero';
-
 \$a->config['system']['db_log'] = '/logs/db.log';
 
 error_reporting( E_ALL & ~E_NOTICE );
@@ -51,10 +45,6 @@ ini_set('error_log','/logs/php.log');
 ini_set('log_errors','1');
 ini_set('display_errors', '0');
 EOF
-fi
-
-if [ "$ENABLE_HTACCESS"x = 'Truex' ]; then
-    /enable_htaccess.sh
 fi
 
 if [ ! -f /.friendica_admin_created ]; then
