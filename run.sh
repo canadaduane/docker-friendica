ca1e386200ac2e8f4ef8af7191d607cee119319a
Duane Johnson <duane.johnson@gmail.com>
2014-09-04 22:41:32 -0600

    Setup db in setup.sh

--- run.sh
+++ run.sh
@@ -3,20 +3,9 @@ if [ "$ENABLE_HTACCESS"x = 'Truex' ]; then
     /enable_htaccess.sh
 fi
 
-#!/bin/bash
-
-VOLUME_HOME="/var/lib/mysql"
-
-sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
-if [[ ! -d $VOLUME_HOME/mysql ]]; then
-    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
-    echo "=> Installing MySQL ..."
-    mysql_install_db > /dev/null 2>&1
-    echo "=> Done!"  
+if [ ! -f /.mysql_admin_created ]; then
     /create_mysql_admin_user.sh
-else
-    echo "=> Using an existing volume of MySQL"
+    touch /.mysql_admin_created
 fi
 
 if [ ! -f /.friendica_admin_created ]; then
